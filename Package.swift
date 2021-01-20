2114a349a41b83f87a42c5ab4d5c8ac1391793d3
Jon Willis <jondwillis@gmail.com>
2021-01-19 22:18:31 -0500

    Updating Realm to 10.5.x+

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "Unrealm", targets: ["Unrealm", "UnrealmObjC"])
     ],
     dependencies: [
-        .package(url: "https://github.com/realm/realm-cocoa.git", from: "5.0.3"),
+        .package(url: "https://github.com/realm/realm-cocoa.git", from: "10.5.0"),
         .package(url: "https://github.com/arturdev/RuntimeNew.git", from: "2.1.7")
     ],
     targets: [
